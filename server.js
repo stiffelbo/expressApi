07f4406c2e38f7cea4e9e9a9e8eedfde257dc897
stiffelbo <partygrouppl@gmail.com>
2021-06-20 00:15:54 +0200

    29.6 Atlas db error

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use((req, res) => {
 });
 
 // connects our backend code with the database
-mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://stiffelbo:stifelboMongo123@cluster0.vgspf.mongodb.net/newWaveDb?retryWrites=true&w=majority', { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
